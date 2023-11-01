a6155d785b41c579d75a8aa349c34b26cbaf05fc
lihaji <835510106@qq.com>
2023-11-01 19:36:44 +0800

    update

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,229 +1,254 @@
 #include <iostream>
+#include <string>
 #include <set>
-#include <algorithm>
+#include <iomanip>
+#include <fstream>
+#include <map>
+
+#define INF 0x3f3f3f
 using namespace std;
-#define INF 0x3f3f3f3f
+const int maxn = 505;
 
-int m;
-string mp[20];
-const int maxn = 200;
+// 定义遍历邻接节点的方向
+int directions[8][2] = {
+    {0, -1},  // 上
+    {0, 1},   // 下
+    {-1, 0},  // 左
+    {1, 0},   // 右
+    {-1, -1}, // 左上
+    {1, -1},  // 右上
+    {-1, 1},  // 左下
+    {1, 1},   // 右下
 
-int directions[4][2] = {
-    {0, -1}, // 上
-    {0, 1},  // 下
-    {-1, 0}, // 左
-    {1, 0}   // 右
 };
 
+// 定义节点的结构
 struct Node
 {
-    // 定义构造函数
-    Node() {}
-    Node(int x, int y)
-    {
-        this->x = x;
-        this->y = y;
-        this->index = x * m + y;
-        this->father = 0;
-    }
+    // 当前节点的状态
+    // 0 - new
+    // 1 - open
+    // -1 - close
+    int state;
+
+    // 当前节点的坐标值
     int x;
     int y;
-    int index;
+
+    // 当前节点的 h 值（也就是 dijkstra 中的 dis ）
+    double h;
+
+    // 当前节点的 k 值
+    double k;
+
+    // 当前节点指向的父节点
     int father;
+
+    // 当前节点的下标
+    int index;
 };
 
+// 节点列表
 Node node[maxn];
 
-Node s, e;
+// 地图信息
+string mp[maxn];
 
-// 定义评价函数
-int dis[maxn];
+// 地图维数
+int n;
 
-// 定义开放列表和关闭列表
-set<int> OpenList;
-set<int> CloseList;
+map<int, bool> isPath;
 
-// 下标转为坐标
-pair<int, int> indexToCoordinates(int index)
+// OpenList & CloseList
+set<Node *> OpenList;
+set<Node *> CloseList;
+
+// 初始化
+void init()
 {
-    int x = index / m;
-    int y = index % m;
-    return pair<int, int>(x, y);
+    // n * n 大小的栅格化地图
+    cin >> n;
+    // 输入地图数据
+    for (int i = 0; i < n; i++)
+        cin >> mp[i];
+
+    // 初始化节点数据
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            node[cnt].father = -1;
+            node[cnt].h = INF;
+            node[cnt].k = INF;
+            node[cnt].state = 0;
+            node[cnt].x = i;
+            node[cnt].y = j;
+            node[cnt].index = cnt;
+            cnt++;
+        }
+    }
+    // node[0].h = 0;
+    // node[0].k = 0;
+
+    node[99].h = 0;
+    node[99].k = 0;
 }
 
-// 坐标转为下标
-int CoordinatesToindex(int x, int y)
-{
-    return x * m + y;
-}
-
-// 计算两个点之间的曼哈顿距离
-int manhattanDistance(int point1, int point2)
-{
-    pair<int, int> p1 = indexToCoordinates(point1);
-    pair<int, int> p2 = indexToCoordinates(point2);
-    return abs(p1.first - p2.first) + abs(p1.second - p2.second);
-}
-
-// 判断节点是否可达
 bool isValidNode(int x, int y)
 {
-    if (x < 0 || x >= m || y < 0 || y >= m)
+    if (x < 0 || x >= n || y < 0 || y >= n)
         return false;
     if (mp[x][y] == '#')
         return false;
+
     return true;
 }
 
-// 输出开放列表中的元素
-void print_OpenList()
+// 使用 dijkstra 规划最短路径
+void dijkstra(int s, int e)
 {
-    cout << "OpenList: ";
-    for (set<int>::iterator i = OpenList.begin(); i != OpenList.end(); i++)
-    {
-        cout << *i << ' ';
-    }
-    cout << endl;
-}
+    // 将起点 S 放入开放列表
+    OpenList.insert(&node[s]);
+    node[s].state = 1;
 
-void Dijkstra()
-{
-    // 起点加入开放列表
-    OpenList.insert(0);
     while (OpenList.size())
     {
-        if (OpenList.find(e.index) != OpenList.end())
+        // 遍历 OpenList，找到 h 值最小的节点 n
+        int min_h_value = INF;
+        // 找到当前正在处理的节点的指针
+        Node *N_ptr;
+        for (set<Node *>::iterator i = OpenList.begin(); i != OpenList.end(); i++)
         {
-            cout << "find" << endl;
-            return;
-        }
-
-        // 遍历开放列表，找到 dis 的值最小的节点
-        int min_f = INF;
-        int n_ind = -1;
-
-        for (set<int>::iterator i = OpenList.begin(); i != OpenList.end(); i++)
-        {
-            if (dis[*i] < min_f)
+            Node *nodePtr = *i;
+            if (nodePtr->h < min_h_value)
             {
-                min_f = dis[*i];
-                n_ind = *i;
+                min_h_value = nodePtr->h;
+                N_ptr = nodePtr;
             }
         }
 
-        // 当前正在处理的节点 N
-        Node n = node[n_ind];
-        // 遍历与 N 相邻的节点 N_near
-
-        for (int j = 0; j < 4; j++)
+        // 遍历 N 的邻接节点
+        for (int i = 0; i < 8; i++)
         {
-            int n_near_x = node[n_ind].x + directions[j][1];
-            int n_near_y = node[n_ind].y + directions[j][0];
-            int n_near_ind = CoordinatesToindex(n_near_x, n_near_y);
-            if (!isValidNode(n_near_x, n_near_y) || CloseList.find(n_near_ind) != CloseList.end())
-                ;
-            // 节点没有访问过
-            else if (OpenList.find(n_near_ind) == OpenList.end())
+            // 先判断当前节点是否存在（合法）
+            int n_near_x = N_ptr->x + directions[i][0];
+            int n_near_y = N_ptr->y + directions[i][1];
+            if (!isValidNode(n_near_x, n_near_y))
+                continue;
+
+            Node *N_near_Ptr = &node[n_near_x * n + n_near_y];
+
+            // 当前节点的状态为 new（没有访问过）
+            if (N_near_Ptr->state == 0)
             {
                 // 加入开放列表
-                OpenList.insert(n_near_ind);
-                // 更新 dis 数组
-                dis[n_near_ind] = dis[n.index] + 1;
-                // 将 n 设为 n_near 的父结点
-                node[n_near_ind].father = n.index;
+                OpenList.insert(N_near_Ptr);
+                N_near_Ptr->state = 1;
+                // 更新 h 值
+                N_near_Ptr->h = N_ptr->h + (i > 3 ? 1.4 : 1);
+                // 将 n 设置为 n_near 的父结点
+                N_near_Ptr->father = N_ptr->index;
             }
-            // 节点已经访问过，但是没有找到最短的路径
-            else if (OpenList.find(n_near_ind) != OpenList.end())
+            // 当前节点的状态为 open（已经访问过但是还没有确定最短路径）
+            else if (N_near_Ptr->state == 1)
             {
-                if (dis[n.index] + 1 < dis[n_near_ind])
+                double new_h = N_ptr->h + (i > 3 ? 1.4 : 1);
+                if (N_near_Ptr->h > new_h)
                 {
-                    dis[n_near_ind] = dis[n.index] + 1;
-                    node[n_near_ind].father = n.index;
+                    N_near_Ptr->h = new_h;
+                    N_near_Ptr->father = N_ptr->index;
                 }
             }
+            // 当前节点处理完毕，移入CloseList
+            N_ptr->state = -1;
+            OpenList.erase(N_ptr);
+            CloseList.insert(N_ptr);
+            // 终点进入 CloseList 算法结束
+            if (N_ptr->index == e)
+                return;
         }
-        OpenList.erase(n.index);
-        CloseList.insert(n.index);
     }
 }
 
-void init()
+// 输出最终确定的最短路径
+void outPut_Path()
 {
-    cin >> m;
-    int x, y;
-    // 输入起点和终点的坐标
-    cin >> x >> y;
-    s = Node(x, y);
-    cin >> x >> y;
-    e = Node(x, y);
-    // 输入地图数据
-    for (int i = 0; i < m; i++)
-        cin >> mp[i];
+    ofstream outputFile("output.md", ios::app);
 
-    memset(dis, INF, sizeof(dis));
-
-    dis[0] = 0;
-
-    for (int i = 0; i < m * m; i++)
+    int index = 99;
+    while (index != 0)
     {
-        node[i].index = i;
-        node[i].x = indexToCoordinates(i).first;
-        node[i].y = indexToCoordinates(i).second;
-        node[i].father = -1;
+        isPath[index] = true;
+        index = node[index].father;
     }
+
+    outputFile << "## Path" << endl;
+    outputFile << "```" << endl;
+
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (isPath[cnt])
+                outputFile << "·" << ' ';
+            else
+                outputFile << mp[i][j] << ' ';
+            cnt++;
+        }
+        outputFile << endl;
+    }
+    outputFile << "```" << endl;
 }
 
-void solve()
+// 输出 H 权值图
+void outPut_H_graph()
 {
-    init();
-    Dijkstra();
-
-    // 输出访问过的节点
-    cout << "被访问过的节点:" << endl;
-    for (int i = 0; i < m; i++)
+    ofstream outputFile("output.md", ios::app);
+    outputFile << "## H-Graph" << endl;
+    for (int i = -1; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        if (i >= 0)
+            outputFile << "|" << i;
+        else
+            outputFile << "|/";
+    }
+
+    outputFile << "|" << endl;
+    for (int i = -1; i < n; i++)
+        outputFile << "|-";
+    outputFile << "|" << endl;
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        outputFile << "|" << i << "|";
+        for (int j = 0; j < n; j++)
         {
-            if (CloseList.find(CoordinatesToindex(i, j)) == CloseList.end())
-                cout << mp[i][j] << ' ';
+            if (node[cnt].h == INF)
+                outputFile << "###" << '|';
             else
-                cout << 'A' << ' ';
+                outputFile << fixed << setprecision(1) << node[cnt].h << '|';
+            cnt++;
         }
-        cout << endl;
+        outputFile << endl;
     }
+    outputFile << endl;
+}
 
-    // 输出最短路径
-    cout << endl
-         << "最短路径:" << endl;
-    set<int> path;
-    int p = 99;
-    while (p != 0)
-    {
-        path.insert(p);
-        p = node[p].father;
-    }
-
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (path.find(CoordinatesToindex(i, j)) == path.end())
-                cout << mp[i][j] << ' ';
-            else
-                cout << '*' << ' ';
-        }
-        cout << endl;
-    }
+void outPut()
+{
+    ofstream outputFile("output.md");
+    outputFile << "# OutPuts" << endl;
+    outputFile << endl;
+    outPut_H_graph();
+    // outPut_Path();
 }
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-#ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    solve();
-    cout << "";
+    init();
+    dijkstra(99, 0);
+    outPut();
 }
